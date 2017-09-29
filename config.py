a19c004480743e8e08ded3e26034f87a002734ae
drewkerrigan <drew@kerrigan.io>
2017-09-29 19:52:47 -0400

    Don't log stats connections

--- config.py
+++ config.py
@@ -83,6 +83,7 @@ listen stats
   balance
   mode http
   stats enable
+  option dontlog-normal
   monitor-uri /_haproxy_health_check
   acl getpid path /_haproxy_getpids
   http-request use-service lua.getpids if getpid
